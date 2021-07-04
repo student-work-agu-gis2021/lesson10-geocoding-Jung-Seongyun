792ba41394b0b7bb5bb391ae489fa0436e36c770
JungSeongyun <81940671+Jung-Seongyun@users.noreply.github.com>
2021-07-04 16:28:20 +0000

    Fixed the error.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -10,10 +10,10 @@
 import geopandas as gpd
 import pandas as pd
 # Read the data (replace "None" with your own code)
-
+data = None
 # YOUR CODE HERE 1 to read the data
-data = pd.read_csv('shopping_centers.txt',sep=';')
-
+data=pd.read_table('shopping_centers.txt', sep=';', header=None)
+data.columns=['id','name', 'addr']
 #TEST COEE
 # Check your input data
 print(data)
@@ -26,7 +26,7 @@ from geopandas.tools import geocode
 
 # Geocode addresses using Nominatim. Remember to provide a custom "application name" in the user_agent parameter!
 #YOUR CODE HERE 2 for geocoding
-geo = geocode(data['addr'], provider = 'nominatim', user_agent = 'autogis_xx', timeout = 4)
+geo = geocode(data['addr'], provider='nominatim', user_agent='autogis_xx')
 
 #TEST CODE
 # Check the geocoded output
@@ -35,7 +35,6 @@ print(geo)
 #TEST CODE
 # Check the data type (should be a GeoDataFrame!)
 print(type(geo))
-
 
 # Check that the coordinate reference system of the geocoded result is correctly defined, and **reproject the layer into JGD2011** (EPSG:6668):
 
@@ -46,34 +45,30 @@ geo = geo.to_crs(6668)
 # Check layer crs
 print(geo.crs)
 
-
 # YOUR CODE HERE 4 to join the tables
-#geodata = None
-geodata = geo.join(data)
+geodata = None
+geodata =geo.join(data)
 
 #TEST CODE
 # Check the join output
 print(geodata.head())
-
 
 # - Save the output as a Shapefile called `shopping_centers.shp` 
 
 # Define output filepath
 out_fp = None
 # YOUR CODE HERE 5 to save the output
-out_fp = "data/shopping_centers.shp"
+out_fp = r"shopping_centers.shp"
 geodata.to_file(out_fp)
 
 # TEST CODE
 # Print info about output file
 print("Geocoded output is stored in this file:", out_fp)
 
-
 # ## Problem 2: Create buffers around shopping centers
 # 
 # Let's continue with our case study and calculate a 1.5 km buffer around the geocoded points. 
  
-
 # YOUR CODE HERE 6 to create a new column
 geodata['buffer']=None
 
@@ -88,20 +83,17 @@ print(geodata.head())
 # Check the data type of the first value in the buffer-column
 print(type(geodata.at[0,'buffer']))
 
-
 #TEST CODE
 # Check the areas of your buffers in km^2
 print(round(gpd.GeoSeries(geodata["buffer"]).area / 1000000))
 
-
 # - Replace the values in `geometry` column with the values of `buffer` column:
 
 # YOUR CODE HERE 8 to replace the values in geometry
-geodata['geometry'] = geodata['buffer']
+geodata['geometry'] =geodata['buffer']
 
 #TEST CODE
 print(geodata.head())
-
 
 # ## Problem 3: How many people live near shopping centers? 
 # 
@@ -109,30 +101,38 @@ print(geodata.head())
 # 
 
 # YOUR CODE HERE 9
+pop=None
 # Read population grid data for 2018 into a variable `pop`. 
-pop = gpd.read_file('data/500m_mesh_suikei_2018_shape_13/500m_mesh_2018_13.shp')
-
-from pyproj import CRS
-pop.crs = CRS.from_epsg(4612).to_wkt()
+pop=gpd.read_file(r"data/500m_mesh_suikei_2018_shape_13/500m_mesh_2018_13.shp")
+pop = pop[["PTN_2020", "geometry"]]
 geodata = geodata.to_crs(pop.crs)
+print(pop.crs)
+print(geodata.crs)
 
 #TEST CODE
 # Check your input data
 print("Number of rows:", len(pop))
 print(pop.head(3))
 
-
 # In[ ]:
-
 
 # Create a spatial join between grid layer and buffer layer. 
 # YOUR CDOE HERE 10 for spatial join
-join = gpd.sjoin(geodata, pop, how = "inner", op = "intersects")
+
+join = gpd.sjoin(geodata, pop, how="inner", op="intersects")
+tokyu = join.loc[join["name"] == "Tokyu Department Store"]
+seibu = join.loc[join["name"] == "Seibu Shibuya Store"]
+azabu = join.loc[join["name"] == 'National Azabu']
 
 # YOUR CODE HERE 11 to report how many people live within 1.5 km distance from each shopping center
-grouped = join.groupby('name')
-for key, group in grouped:
- print(round(sum(group['PTN_2020'])),"people live within 1.5km:from",key)
+tokyu_sum=round(tokyu["PTN_2020"].sum())
+seibu_sum=round(seibu["PTN_2020"].sum())
+azabu_sum=round(azabu["PTN_2020"].sum())
+
+print("Tokyu Department Store :"+str(tokyu_sum))
+print("Seibu Shibuya Store :"+str(seibu_sum))
+print("National Azabu :"+str(azabu_sum))
+
 # **Reflections:**
 #     
 # - How challenging did you find problems 1-3 (on scale to 1-5), and why?
@@ -140,6 +140,11 @@ for key, group in grouped:
 # - What was difficult?
 
 # YOUR ANSWER HERE
-#Most problems were difficult for me.
+# Problem1:4 Problem2:5 Problem3:5
+#It was difficult because I didn't know how to make an address from problem 1.
+
+#Problem1
+
+#How to make an address
 
 # Well done!
